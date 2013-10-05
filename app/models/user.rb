807e8b86be94b34b69c474c8147678c0ed7eb1aa
Xavier Denis <xldenis@gmail.com>
2013-10-05 15:49:43 -0400

    Switch from using username to uid

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,14 +7,14 @@ class User
   field :expired_at
 
   def self.find_or_create_by_auth(auth_hash)
-    @user = User.where(identifier: auth_hash["username"], provider: auth_hash["provider"]).first
+    @user = User.where(identifier: auth_hash["uid"], provider: auth_hash["provider"]).first
 
     if @user.nil?
       @user = User.new
       @user.provider = auth_hash["provider"]
       @user.token = auth_hash.credentials["token"]
       @user.expired_at = auth_hash.credentials["expires_at"]
-      @user.username = auth_hash["username"]
+      @user.identifier = auth_hash["uid"]
       @user.save
     end
     @user
