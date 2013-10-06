dc34df503586f16a4351c05eac8617d70b94b090
Xavier Denis <xldenis@gmail.com>
2013-10-05 22:34:28 -0400

    Add set of tags to user object

--- app/models/post.rb
+++ app/models/post.rb
@@ -8,6 +8,40 @@ class Post
 
   field :metrics, type: Array,default: []
 
-  field :tags, type: Hash
+  field :tags, type: Hash, default: {}
 
+  field :units, type: Array,default: []
+
+  validates_presence_of :user
+
+  before_create :process_tags
+
+
+
+  #returns posts on the given page
+  def self.page(page,limit)
+    page ||= 0
+    limit ||= 25
+    order_by(:created_at.desc).skip(page.to_i*limit.to_i).limit(limit.to_i)
+  end 
+
+  #returns posts with the given tag
+  def self.find_with_tag tag
+    exists("tags.#{tag}" => true)
+  end
+
+  # returns metrics of a post that contain the given unit
+  def metric_with_unit(unit)
+    metrics.select {|m| Unit.new(m) <=> Unit.new(unit)}
+  end
+
+  private
+
+  def process_tags
+    unit_hash = Hash[units.map {|e| [e,true]}]
+    user.units |= units
+    user.tags.merge! tags
+    user.save
+    tags.merge! unit_hash
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,20 +6,10 @@ class User
   field :secret
   field :expired_at
   field :identifier
-  has_many :posts do
-    def page(page,limit)
-      page ||= 0
-      limit ||= 25
-      order_by(:created_at.desc).skip(page.to_i*limit.to_i).limit(limit.to_i)
-    end 
-    def find_with_tag tag
-      exists("tags.#{tag}" => true)
-    end
 
-    def metric_with_unit(unit)
-      metrics.select {|m| Unit.new(m) <=> Unit.new(unit)}
-    end
-  end
+  field :units, type: Array, default: []
+  field :tags, type: Hash, default: {}
+  has_many :posts 
 
   def self.find_or_create_by_auth(auth_hash)
     @user = User.where(identifier: auth_hash["uid"], provider: auth_hash["provider"]).first
